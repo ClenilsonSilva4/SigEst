5c7f918337a37c9ec80d7af66db0c7471cd11fc3
Clenilson Silva <clenilson.silva4@hotmail.com>
2021-11-10 14:36:35 -0300

    Refactoring

---
+++ src/server/Server.java
@@ -0,0 +1,60 @@
+package server;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.text.SimpleDateFormat;
+
+public class Server{
+    public static void main(String[] args) throws Exception{
+        //Criação e associação do socket ao endereço do servidor
+        ServerSocket serverSocket = new ServerSocket(8080, 100, InetAddress.getByName(getRedeIP()));
+        //Mostra as informações do socket do servidor
+        System.out.println("Servidor iniciado no endereço " + serverSocket.getInetAddress());
+        System.out.println("Agurdando conexão na porta: " + serverSocket.getLocalPort());
+        System.out.println();
+        
+        //Laço infinito para aceitar as conexões ao servidor e inicia uma thread com o cliente
+        while(true){
+            final Socket activeSocket = serverSocket.accept();
+
+            System.out.println("Conexão recebida de " + activeSocket);
+            ServerHandler newConnection = new ServerHandler(activeSocket);
+            newConnection.run(); // start a new thread
+        }
+    }
+
+    //Função para conseguir o endereço IP principal da rede
+    public static String getRedeIP() {
+        String atualIP = null;
+
+        Enumeration<NetworkInterface> interfacesRede;
+            try {
+                interfacesRede = NetworkInterface.getNetworkInterfaces();
+
+                //Enquanto a interface tem elementos, testa para ver se encontra um que se adequa
+                while (interfacesRede.hasMoreElements()) {
+                    NetworkInterface ni = interfacesRede.nextElement();
+                    Enumeration<InetAddress> address = ni.getInetAddresses();
+                    while (address.hasMoreElements()) {
+                        InetAddress addr = address.nextElement();
+                        if (!addr.isLoopbackAddress() && addr.isSiteLocalAddress()
+                                && !addr.getHostAddress().contains(":")) {
+                            atualIP  = addr.getHostAddress();
+                        }
+                    }
+                }
+                //Caso o laço não resulte em nenhum resultado, define o IP como o localhost
+                if (atualIP  == null) {
+                    atualIP  = "127.0.0.1";
+                }
+
+            } catch (SocketException e) {
+                //Caso haja alguma exceção, o IP é definido como localhost
+                atualIP  = "127.0.0.1";
+            }
+
+        //Retorna o IP
+        return atualIP ;
+    }
+}
--- src/server/ServerHandler.java
+++ src/server/ServerHandler.java
@@ -2,9 +2,14 @@ package server;
 
 import java.io.*;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ServerHandler implements Runnable{
     private final Socket clientSocket;
+    private Date tempoLocal;
+    private final SimpleDateFormat ft = new SimpleDateFormat("HH:mm:ss");
+    private String destinationID;
 
     public ServerHandler(Socket clientSocket) {
         this.clientSocket = clientSocket;
@@ -12,27 +17,54 @@ public class ServerHandler implements Runnable{
 
     @Override
     public void run() {
-        try {
-            handleClientSocket();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        handleClientRequest();
     }
 
-    private void handleClientSocket() throws IOException {
-        InputStream inputStream = clientSocket.getInputStream();
-        PrintWriter messageWriter = new PrintWriter(clientSocket.getOutputStream());
-        BufferedReader messageReader = new BufferedReader(new InputStreamReader(inputStream));
+    //Função para lidar com a conexão do cliente
+    public void handleClientRequest(){
+        String inMsg, outMsg, identifier = clientSocket.getInetAddress().toString();
+        BufferedReader socketReader;
+        BufferedWriter socketWriter;
 
-        String line;
+        try{
+            //Inicialização dos buffers de leitura e escrita para se comunicar com o cliente
+            socketReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            socketWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
 
-        while ((line = messageReader.readLine()) != null) {
-            if ("quit".equalsIgnoreCase(line)) {
-                break;
+            tempoLocal = new Date();
+
+            //Adicionando o cliente na lista de clientes
+            System.out.println(identifier + " se conectou.");
+
+            //Laço para ficar lendo as mensagens do cliente da thread e transmitir para os demais clientes conectados
+            while(true){
+                inMsg = socketReader.readLine();
+                tempoLocal = new Date();
+
+                if(!(clientSocket.isConnected())){
+                    throw new Exception();
+                }else if(!(inMsg.equalsIgnoreCase("null"))){
+                    socketWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                    outMsg = "Servidor: mensagem " + inMsg + " recebida às: " + ft.format(tempoLocal);
+                    socketWriter.write(outMsg);
+                    socketWriter.write("\n");
+                    socketWriter.flush();
+
+                    //Adicionar função para poder encerrar a conexão com o cliente.
+                    if(inMsg.equalsIgnoreCase("sair")){
+                        System.out.println("Conexão encerrada pelo cliente");
+                        break;
+                    }
+
+                    System.out.println(identifier + ": " + inMsg);
+
+                    //Salvar a mensagem no BD para os usuários receberem
+
+                }
             }
-
-            //function to save the message to the DB
+            clientSocket.close();
+        }catch (Exception e){
+            e.printStackTrace();
         }
-        clientSocket.close();
     }
 }
